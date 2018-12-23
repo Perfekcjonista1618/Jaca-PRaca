84e3b45e9e8278758ecd2ea5a47b6285d1206536
Perfekcjonista1618 <armiq20@gmail.com>
2018-12-23 13:24:55 +0100

    Upload
    update

---
+++ BOT/TwtichPracaBot/KonsolowySzczur/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib;
+using TwitchLib.Models.API;
+namespace KonsolowySzczur
+{
+    class Program
+    {
+        private static TwitchAPI api = new TwitchLib.TwitchAPI("o9atkbd9c8z95a2955e8zs1vv8pp7o", "qn73zxxaxf7pggww2uv3l3mtjy77he");
+        static void Main(string[] args)
+        {
+            //private static TwitchLib.TwitchAPI api;
+            Connect();
+            Console.ReadLine();
+        }
+        public static async void Connect()
+        {
+            var id = await api.Channels.v5.GetChannelAsync();
+            var channelFollowers = await api.Channels.v5.GetChannelFollowersAsync(id.Id);
+            Console.WriteLine(channelFollowers.Total.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BOT/TwtichPracaBot/TwtichPracaBot/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TwitchLib;
+using TwitchLib.Models.API;
+
+namespace TwtichPracaBot
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private static TwitchLib.TwitchAPI api;
+        //private static 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Connect();
+            //Task t = Task.Run(async () =>
+            //{
+            //    //api = new TwitchLib.TwitchAPI("rxz5pppetn8rety2xl0jq6ighu8h4m", "pmnj871u2ddlmovup6ousnflfboqka");
+
+            //    var channel = await api.Channels.v5.GetChannelAsync();
+            //    var allSubscriptions = await api.Channels.v5.GetAllSubscribersAsync(channel.Id);
+            //    bool isStreaming = await api.Streams.v5.BroadcasterOnlineAsync(channel.Id);
+            //    MessageBox.Show(isStreaming.ToString());
+            //    //foreach (var item in allSubscriptions)
+            //    //{
+            //    //    Console.WriteLine("Uzytkownik {0}, ID: {1}",item.User.Name, item.Id);
+            //    //}
+            //});
+            //t.Wait();
+        }
+        public async void Connect()
+        {
+            TwitchAPI api = new TwitchLib.TwitchAPI("o9atkbd9c8z95a2955e8zs1vv8pp7o", "qn73zxxaxf7pggww2uv3l3mtjy77he");
+            var id = await api.Channels.v5.GetChannelAsync();
+            MessageBox.Show(id.Followers.ToString(),"Korona Kielce Kurwa Widelce" ,MessageBoxButton.OK);
+  
+        }
+    }
+}
